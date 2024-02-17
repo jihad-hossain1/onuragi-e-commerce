e79df1f1505594ba7074615c700815e99f486960
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-17 22:59:07 +0600

    product add api with get all products

--- app/api/v1/products/route.js
+++ app/api/v1/products/route.js
@@ -1,40 +1,41 @@
 import Product from "@/src/models/product.models";
+import { validateJSON } from "@/utils/validateJSON";
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
   const reqBody = await req.json();
   try {
-    const { name, image, categoryID, price } = reqBody;
+    // checker for anyone can send undefine or {} or null value in api requiest
+    const isValid = validateJSON(reqBody);
 
-    if (!(name && image && categoryID && price)) {
-      return NextResponse.json({ message: "All field are empty" });
+    if (isValid === true) {
+      const { name, image, categoryID, price } = reqBody;
+
+      // if price is 0 then show error
+      if (price === 0) {
+        return NextResponse.json({ message: "price field are empty" });
+      }
+
+      const newProduct = new Product({ name, image, categoryID, price });
+      const product = await newProduct.save();
+
+      return NextResponse.json({
+        message: "product has created",
+        status: 201,
+        product,
+      });
+    } else {
+      return NextResponse.json({ message: "inavlid fields: ", isValid });
     }
-
-    if (name === "") {
-      return NextResponse.json(
-        { message: "name field required" },
-        { status: 400 }
-      );
-    } else if (image === "") {
-      return NextResponse.json({ message: "image Link field are empty" });
-    } else if (categoryID === "") {
-      return NextResponse.json({ message: "categoryID field are empty" });
-    } else if (price === 0) {
-      return NextResponse.json({ message: "price field are empty" });
-    }
-
-    const newProduct = new Product({ name, image, categoryID, price });
-    const product = newProduct.save();
-
-    return NextResponse.json({
-      message: "product has created",
-      status: 201,
-      product,
-    });
   } catch (error) {
-    return NextResponse.json(reqBody);
+    return NextResponse.json(error);
   }
+}
+
+export async function GET(req, res) {
   try {
+    const products = await Product.find();
+    return NextResponse.json(products);
   } catch (error) {
     return NextResponse.json(error);
   }
---
+++ utils/validateJSON.js
@@ -0,0 +1,30 @@
+export function validateJSON(jsonObj) {
+  let invalidFields = {};
+  for (let key in jsonObj) {
+    if (!jsonObj[key] && jsonObj[key] !== 0) {
+      let invalidType;
+      switch (typeof jsonObj[key]) {
+        case "undefined":
+          invalidType = "undefined";
+          break;
+        case "object":
+          invalidType = "null";
+          break;
+        default:
+          invalidType = "empty";
+          break;
+      }
+      invalidFields[key] = invalidType;
+    }
+  }
+  return Object.keys(invalidFields).length === 0 ? true : invalidFields;
+}
+// export function validateJSON(jsonObj) {
+//   let invalidFields = [];
+//   for (let key in jsonObj) {
+//     if (!jsonObj[key] && jsonObj[key] !== 0) {
+//       invalidFields.push(key);
+//     }
+//   }
+//   return invalidFields.length === 0 ? true : invalidFields;
+// }
