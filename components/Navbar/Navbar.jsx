0c81b2312d4eeb5504fdfdd209fa6b842c3f7cd3
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-20 14:06:18 +0600

    navbar icons added

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,28 +2,55 @@
 
 import Link from "next/link";
 import React from "react";
-// import { getServerSession } from "next-auth";
-// import { options } from "@/app/api/auth/[...nextauth]/options";
 import { signOut, useSession } from "next-auth/react";
+import { SlMagnifier } from "react-icons/sl";
+import { HiOutlineShoppingBag, HiOutlineUserCircle } from "react-icons/hi2";
 
 const Navbar = () => {
   const { status } = useSession();
-  // const session = await getServerSession(options);
-  return (
-    <div className="flex items-center gap-7">
-      <Link href={"/"}>Home</Link>
 
-      {status === "authenticated" ? (
-        <button onClick={() => signOut()}>logOut</button>
-      ) : (
-        <Link href="/login">Login</Link>
-      )}
-      {/* {session ? (
-        <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
-      ) : (
-        <Link href="/api/auth/signin">Login</Link>
-      )} */}
-    </div>
+  return (
+    <main className="max-w-screen-xl p-1 mx-auto">
+      <div className="flex items-center gap-4 justify-between">
+        {/* logo section */}
+        <section>Onuragi</section>
+
+        {/* navmenu section  */}
+        <section className="flex items-center gap-10">
+          <Link href={"/"} className="uppercase">
+            Home
+          </Link>
+          <Link href={"/"} className="uppercase">
+            Shop
+          </Link>
+          <Link href={"/"} className="uppercase">
+            Product
+          </Link>
+          <Link href={"/"} className="uppercase">
+            Buy
+          </Link>
+        </section>
+
+        {/* icon section  */}
+        <section>
+          <button>
+            <SlMagnifier />
+          </button>
+
+          <button>
+            <HiOutlineShoppingBag />
+          </button>
+          <button>
+            <HiOutlineUserCircle />
+          </button>
+          {status === "authenticated" ? (
+            <button onClick={() => signOut()}>logOut</button>
+          ) : (
+            <Link href="/login">Login</Link>
+          )}
+        </section>
+      </div>
+    </main>
   );
 };
 
