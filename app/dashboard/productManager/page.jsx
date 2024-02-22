a36b3475002ef833658a5640e6e6d74a9c40d72e
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-22 18:31:48 +0600

    some error fix

--- app/dashboard/productManager/page.jsx
+++ app/dashboard/productManager/page.jsx
@@ -1,13 +1,14 @@
 import Action from "@/components/Dashboard/productMange/action/Action";
-import { getAllCategory } from "@/utils/fetch/product";
+import { getAllCategory, getSubCategories } from "@/utils/fetch/product";
 
 const ProductManagerpage = async () => {
   const categories = await getAllCategory();
+  const subcategories = await getSubCategories();
 
   return (
     <main className="flex flex-col gap-5">
       <Action />
-      <div>
+      <>
         {/* category section  */}
         <div className="flex flex-col gap-3">
           <h4>Total Categories: {categories?.length || 0} </h4>
@@ -20,7 +21,21 @@ const ProductManagerpage = async () => {
             ))}
           </div>
         </div>
-      </div>
+      </>
+      <>
+        {/* sub-category section  */}
+        <div className="flex flex-col gap-3">
+          <h4>Total SubCategories: {subcategories?.length || 0} </h4>
+          <div>
+            {subcategories?.map((category, _ind) => (
+              <h4 key={category?._id} className="flex items-center gap-2">
+                <span>{_ind + 1}.</span>
+                <span>{category?.name}</span>
+              </h4>
+            ))}
+          </div>
+        </div>
+      </>
     </main>
   );
 };
--- utils/fetch/product.js
+++ utils/fetch/product.js
@@ -1,9 +1,26 @@
 "use server";
 
+import axios from "axios";
+
 export const getAllCategory = async () => {
   try {
-    const res = fetch(`${process.env.NEXT_BASE_URL}/category`);
-    const result = (await res).json();
+    const res = axios.get(
+      `https://onuragi-handicarft.vercel.app/api/v1/category`
+    );
+    const result = (await res).data;
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getSubCategories = async () => {
+  try {
+    const res = axios.get(
+      `https://onuragi-handicarft.vercel.app/api/v1/category/subCategory`
+    );
+    const result = (await res).data;
 
     return result;
   } catch (error) {
