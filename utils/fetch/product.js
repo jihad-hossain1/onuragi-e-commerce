69f2fc1083e2d2214e2a7eefc8884e120b0dc2b6
jihad-hossain1 <jihadkhan934@gmail.com>
2024-03-18 22:50:28 +0600

    --chang

--- utils/fetch/product.js
+++ utils/fetch/product.js
@@ -1,35 +1,35 @@
-// "use server";
+"use server";
 
-// import axios from "axios";
+import axios from "axios";
 
-// export const getAllCategory = async () => {
-//   try {
-//     const res = axios.get(`${process.env.NEXT_BASE_URL}/category`);
-//     const result = (await res).data;
-//     // console.log(result);
-//     return result;
-//   } catch (error) {
-//     throw new Error(error.message);
-//   }
-// };
+export const getAllCategory = async () => {
+  try {
+    const res = axios.get(`${process.env.NEXT_BASE_URL}/category`);
+    const result = (await res).data;
+    // console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
-// export const getSubCategories = async () => {
-//   try {
-//     const res = axios.get(`${process.env.NEXT_BASE_URL}/category/subCategory`);
-//     const result = (await res).data;
+export const getSubCategories = async () => {
+  try {
+    const res = axios.get(`${process.env.NEXT_BASE_URL}/category/subCategory`);
+    const result = (await res).data;
 
-//     return result;
-//   } catch (error) {
-//     throw new Error(error.message);
-//   }
-// };
-// export const getProducts = async () => {
-//   try {
-//     const res = axios.get(`${process.env.NEXT_BASE_URL}/products`);
-//     const result = (await res).data;
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+export const getProducts = async () => {
+  try {
+    const res = axios.get(`${process.env.NEXT_BASE_URL}/products`);
+    const result = (await res).data;
 
-//     return result;
-//   } catch (error) {
-//     throw new Error(error.message);
-//   }
-// };
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
